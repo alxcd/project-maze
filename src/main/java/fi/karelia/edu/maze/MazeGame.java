ae8790f3a8323b0c401a9164c72b462b4e1f502f
alex <aleksandr.tsepkovskii@edu.karelia.fi>
2024-03-03 16:50:53 +0200

    added arrow keys handler for player

--- src/main/java/fi/karelia/edu/maze/MazeGame.java
+++ src/main/java/fi/karelia/edu/maze/MazeGame.java
@@ -1,48 +1,102 @@
 package fi.karelia.edu.maze;
 
 import javafx.application.Application;
-import javafx.geometry.Rectangle2D;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
+/**
+ * The type Maze game.
+ */
 public class MazeGame extends Application {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
         launch(args);
     }
 
+    /**
+     * <BorderPane root.Center>
+     *     <StackPane pane>
+     *         <GridPane grid0></GridPane>
+     *         <GridPane grid1></GridPane>
+     *      </StackPane>
+     * </BorderPane>
+     * @param primaryStage
+     */
     @Override
     public void start(Stage primaryStage) {
 
         BorderPane root = new BorderPane();
+        StackPane stackPane = new StackPane();
+        Pane pane = new Pane();
+        pane.getChildren().add(stackPane);
 
-        VBox vBoxLeft = new VBox(new Button("New game"));
-        root.setLeft(vBoxLeft);
+//        AnchorPane root = new AnchorPane();
+//        AnchorPane.setTopAnchor(pane, 10.0);
+//        AnchorPane.setRightAnchor(pane, 10.0);
+//        AnchorPane.setLeftAnchor(pane, 10.0);
+
+
+        root.setCenter(pane);
+        root.setPrefSize(500, 500);
+
+        VBox vBoxLeft = new VBox();
+        vBoxLeft.setPadding(new Insets(15));
+//        AnchorPane.setLeftAnchor(vBoxLeft, 10.0);
+//        AnchorPane.setBottomAnchor(vBoxLeft, 10.0);
+
+        //root.getChildren().addAll(pane, vBoxLeft);
+
+        root.setBottom(vBoxLeft);
 
         LevelMap maze = new LevelMap(10);
 
-        GridPane grid = new GridPane();
-        root.setCenter(grid);
-
+        GridPane grid0 = new GridPane();
         for (int i = 0; i < maze.getMazeMatrix().length; i++) {
             for (int j = 0; j < maze.getMazeMatrix()[i].length; j++) {
-                if (maze.getMazeMatrix()[i][j] == 2) grid.add(new Rectangle(20,20, Color.BLACK), j, i);
-                else grid.add(new Rectangle(20,20, Color.TRANSPARENT), j, i);
+                if (maze.getMazeMatrix()[i][j] == 2) grid0.add(new Rectangle(20,20, Color.BLACK), j, i);
+                else grid0.add(new Rectangle(20,20, Color.TRANSPARENT), j, i);
             }
         }
+
+        GridPane grid1 = new GridPane();
+        //grid1.add(new Circle(5, Color.YELLOW), 1, 1);
+        //grid1.add(new Rectangle(10,10, Color.YELLOW), 11, 11);
+
+        Circle playerCircle = new Circle(5, Color.YELLOW);
+        StackPane.setAlignment(playerCircle, Pos.TOP_LEFT);
+        playerCircle.setCenterX(50);
+        playerCircle.setCenterY(50);
+
+        stackPane.getChildren().addAll(grid0, grid1);
+        pane.getChildren().add(playerCircle);
 
         Scene scene = new Scene(root);
         primaryStage.setTitle("Maze Game");
         primaryStage.setScene(scene);
         primaryStage.show();
 
-        //SceneSizeChangeListener.letterbox(scene, root);
+        scene.setOnKeyPressed(e -> {
+            switch (e.getCode()) {
+                case DOWN: playerCircle.setCenterY(playerCircle.getCenterY() + 10); break;
+                case UP: playerCircle.setCenterY(playerCircle.getCenterY() - 10); break;
+                case LEFT: playerCircle.setCenterX(playerCircle.getCenterX() - 10); break;
+                case RIGHT: playerCircle.setCenterX(playerCircle.getCenterX() + 10); break;
+            }
+        });
+
+
+        SceneSizeChangeListener.letterbox(scene, root);
     }
 }
